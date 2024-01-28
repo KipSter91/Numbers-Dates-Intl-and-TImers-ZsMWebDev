30619e8cde426b83970976718d6b8373cfebf544
Zsolt Marku <zsolt.marku910221@gmail.com>
2024-01-28 07:22:13 +0100

    Math and rounding

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 //1) Converting and Checking Numbers
 
 //In JavaScript, every number is a floating point number. There is no integer type. So, 23 is a floating point number with .0 at the end. So, 23 is equal to 23.0. So, 23===23.0 is true.
-console.log(23===23.0);
+console.log(23 === 23.0);
 
 //Base 10 - 0 to 9. 1/10 = 0.1, 3/10 = 3.3333333333333333
 //Binary base 2 - 0 1
-console.log(0.1+0.2);
-console.log(0.1+0.2===0.3); //false
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3); //false
 
 //Conversion
+//Number is a built in function in JS
 console.log(Number('23'));
 //type coercion - JS automatically converts the number to string
 console.log(+'23'); //same as above
@@ -30,17 +31,83 @@ console.log(Number.parseInt('2.5rem')); //2
 console.log(Number.isNaN(20)); //false
 console.log(Number.isNaN('20')); //false
 console.log(Number.isNaN(+'20X')); //true
-console.log(Number.isNaN(23/0)); //false
+console.log(Number.isNaN(23 / 0)); //false
 
 //isFinite
 //checks if value is a number and not infinity, and not NaN
 console.log(Number.isFinite(20)); //true
 console.log(Number.isFinite('20')); //false
 console.log(Number.isFinite(+'20X')); //false
-console.log(Number.isFinite(23/0)); //false
+console.log(Number.isFinite(23 / 0)); //false
 
 //isInteger
 //checks if value is an integer
 console.log(Number.isInteger(20)); //true
 console.log(Number.isInteger(23.0)); //true
-console.log(Number.isInteger(23/0)); //false
+console.log(Number.isInteger(23 / 0)); //false
+
+//2) Math and Rounding
+//Math is a built in object in JS
+//Math.sqrt
+console.log(Math.sqrt(25)); //5
+console.log(25 ** (1 / 2)); //5 - same as above
+console.log(8 ** (1 / 3)); //2 - cube root
+console.log(25 ** 2); //625
+console.log(8 ** 4); //512
+
+//Math.max
+//returns the maximum value
+console.log(Math.max(5, 18, 23, 11, 2)); //23
+//Math.max converts strings to numbers, as you can see below
+console.log(Math.max(5, 18, '23', 11, 2)); //23
+//Math.max does not parse strings to numbers, as you can see below, we get NaN
+console.log(Math.max(5, 18, '23px', 11, 2)); //NaN
+
+//Math.min
+//returns the minimum value
+console.log(Math.min(5, 18, 23, 11, 2)); //2
+
+//Math.PI
+//returns the value of PI
+//Here below, we are calculating the area of a circle with radius 10
+console.log(Math.PI * Number.parseFloat('10px') ** 2); //314.1592653589793
+
+//Math.random
+//returns a random number between 0 and 1
+console.log(Math.random()); //0.7871914068283662
+
+//below formula calculates a random number between min and max
+const randomNumber = (min, max) => {
+  return (Math.floor(Math.random() * (max - min) + 1) + min)
+}
+
+console.log(randomNumber(5, 19));
+
+//Rounding integers
+//Math.trunc
+//removes the decimal part of the number
+console.log(Math.trunc(25.7));  //25
+
+//Math.round
+//rounds the number to the nearest integer
+console.log(Math.round(25.7));  //26 
+console.log(Math.round(25.4));  //25
+
+//Math.ceil
+//rounds the number up to the nearest integer
+console.log(Math.ceil(25.7));  //26
+console.log(Math.ceil(25.4));  //26
+
+//Math.floor
+//rounds the number down to the nearest integer
+console.log(Math.floor(25.7));  //25
+console.log(Math.floor(25.4));  //25
+
+//Rounding decimals
+//toFixed
+//returns a string
+//rounds the number to the number of decimal places specified
+console.log((2.7).toFixed(0));  //3
+console.log((2.7).toFixed(3));  //2.700
+console.log((2.345).toFixed(2));  //2.35
+console.log(+(2.345).toFixed(2));  //2.35 - converts to number
